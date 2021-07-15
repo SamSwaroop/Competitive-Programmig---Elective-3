93f63cb7701a7dba1ac90edfb43d01af8ab83211
SamSwaroop <samswaroop97@msitprogram.net>
2021-07-15 11:33:48 +0530

    commit

--- 07-set_kth_digit-Python/set_kth_digit.py
+++ 07-set_kth_digit-Python/set_kth_digit.py
@@ -10,33 +10,39 @@ def fun_set_kth_digit(n, k, d):
 	if(n>0):
 		r=(n//(10**k)%10)
 		t=[int(e) for e in str(n)]
-		for i in t:
-			if i==r:
-				y=t.index(i)
-				t[y]=d
+		x=0
+		for i in t[::-1]:
+			if x==k:
+				t[len(t)-x-1]=d
 				f=[str(integers) for integers in t]
 				o="".join(f)
 				return(int(o))
+			x=x+1
 		t.insert(0,d)
 		f=[str(integers) for integers in t]
 		o="".join(f)
 		return(int(o))
 	else:
 		n=abs(n)
-		r=(n//(10**k)%10)
+		# r=(n//(10**k)%10)
+		x=0
 		t=[int(e) for e in str(n)]
 		for i in t:
-			if i==r:
-				y=t.index(i)
-				t[y]=d
+			if x==k:
+				# y=t.index(i)
+				t[len(t)-x-1]=d
 				f=[str(integers) for integers in t]
 				o="".join(f)
 				return(int(o))
+			x=x+1
 		t.insert(0,d)
 		f=[str(integers) for integers in t]
 		o="".join(f)
 		s="-"+o
 		return(int(s))
+
+
+print(fun_set_kth_digit(25452, 3, 9))
 
 	
 
