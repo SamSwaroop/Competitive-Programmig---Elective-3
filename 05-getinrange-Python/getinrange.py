cd91ec8e115bab71fc763870de3218f4f986b051
SamSwaroop <samswaroop97@msitprogram.net>
2021-07-15 12:04:16 +0530

    commit

--- 05-getinrange-Python/getinrange.py
+++ 05-getinrange-Python/getinrange.py
@@ -5,18 +5,44 @@
 # or if x is greater than the upper bound, return the upper bound.
 
 def fun_getinrange(x, bound1, bound2):
-	l=[]
-	l.append(bound1)
-	l.append(bound2)
-	l.sort()
-	s=[]
-	for i in range(l[0],l[1]+1):
-		s.append(i)
-	if(x in s):
+	# l=[]
+	# l.append(bound1)
+	# l.append(bound2)
+	# l.sort()
+	# s=[]
+	# for i in range(l[0],l[1]+1):
+	# 	s.append(i)
+	# if(x in s):
+	# 	return x
+	# elif(x < l[0]):
+	# 	return l[0]
+	# elif(x > l[1]):
+	# 	return l[1]
+
+	lp=0
+	up=0
+	if(bound1 < bound2):
+		lp=bound1
+		up=bound2
+	else:
+		lp=bound2
+		up=bound1
+	if(x<lp):
+		return lp
+	elif(x>up):
+		return up
+	elif(lp<x<up):
 		return x
-	elif(x < l[0]):
-		return l[0]
-	elif(x > l[1]):
-		return l[1]
+	# if(x<bound1):
+	# 		return bound2
+	# elif(x>bound2):
+	# 	return bound1
+	# elif(bound2<x<bound1):
+	# 	return x
+
+
+	
+
+
 
 
