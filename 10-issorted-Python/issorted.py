5bcc4165038d1bf3936cfb725ffe7b4b125b4284
SamSwaroop <samswaroop97@msitprogram.net>
2021-07-28 01:23:24 +0530

    commit

--- 10-issorted-Python/issorted.py
+++ 10-issorted-Python/issorted.py
@@ -6,5 +6,32 @@
 # the list.
 
 def issorted(a):
-	# your code goes here
-	pass
+	test_list=a
+	flag = 0
+	i = 1
+	while i < len(test_list):
+		if(test_list[i] < test_list[i - 1]):
+			flag = 1
+		i += 1
+	if (not flag):
+		return True
+	else :
+		return False
+# d=[1, 2, 3, 4, 5]
+# print(issorted(d))
+# e=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(issorted(e))
+# r=[1]
+# print(issorted(r))
+# t=[1, 1]
+# print(issorted(r))
+# y=[]
+# print(issorted(y))
+# u=[10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+# print(issorted(u))
+# i=[10, 9, 8, 7, 6, 5, 4, 3, 2, 10]
+# print(issorted(i))
+# o=[1, 2, 3, 4, 5.5, 5.1, 7, 8, 9, 10]
+# print(issorted(o))
+
+
