d244aebd9e4d888761849f5e428ce1f95067df62
SamSwaroop <samswaroop97@msitprogram.net>
2021-08-05 04:22:25 +0530

    commit

--- SayHi.py
+++ SayHi.py
@@ -4,5 +4,27 @@
 # sayHi(d1) # Hi, my name is Dot and I am 4 years old!
 # sayHi(d2) # Hi, my name is Elf and I am 3 years old!
 
-class SayHi(Object):
-    pass
+# class SayHi(Object):
+#     pass
+
+class dog():
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    
+    
+    def sayHI(self):
+        if(dog=="d1"):
+            self.name="Dot"
+            self.age="4"
+            return("Hi, my name is"+self.name+"and I am "+self.age+"years old")
+        if(dog=="d2"):
+            self.name="Elf"
+            self.age="3"
+            return("Hi, my name is"+self.name+"and I am "+self.age+"years old")
+
+
+n=dog()
+d1="d1"
+print(n.sayHI(d1))
+        
